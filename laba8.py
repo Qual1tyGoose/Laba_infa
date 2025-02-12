7f0a412f7abbf77dc96510831321c1c21f030efd
Qual1tyGoose <lebedinsergey2017@gmail.com>
2025-02-12 20:18:40 +0300

    laba8

---
+++ laba8.py
@@ -0,0 +1,19 @@
+a = 'ffasgdfut763ghda5sghdg376dty6asdg'
+f = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+s = 0
+stroka = ''
+index = 0
+flag = False
+for i in a:
+    if i in f:
+        stroka += i
+        flag = True
+    else:
+        flag = False
+    if flag:
+        pass
+    else:
+        if stroka:
+            s += int(stroka)
+        stroka = ''
+print(s)
